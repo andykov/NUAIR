49b0f9a558938ba889b4b1afeb2d87744d7e25d2
Andrey Budnikov <andykov.work@gmail.com>
2015-03-24 00:21:03 +0300

    Сраница google карты

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 $(function() {
+	var winH = $(window).height(); 
+
 
 	$('.header__slider').find('.slider').bxSlider({
 		slideSelector: '.slider__item',
@@ -15,49 +17,82 @@ $(function() {
 	// });
 
 	// init content slider
-    $('.about__slider').find('.slider').bxSlider({
-    	pagerCustom: '.about__slider-nav'
-    });
+	$('.about__slider').find('.slider').bxSlider({
+		pagerCustom: '.about__slider-nav'
+	});
 
-    // fixed menu
-    $(window).scroll(function(){
-    	var scroll = $(window).scrollTop();
-  		if (scroll > 150){
-            $(".header__nav").addClass("hide");
-            $(".header__nav").removeClass("header__nav--notbg");
-        };
-  		if (scroll > 160){
-            $(".header__nav").addClass("fixed");
-        };
-        if (scroll < $(".header__nav").height()) {
-        	$(".header__nav").removeClass("hide fixed");
-        	$(".header__nav").addClass("header__nav--notbg");
-        };
-    });
+	// fixed menu
+	var navFixed = $(".header__nav.not-fixed");
+
+	if ($(window).scrollTop()) {
+		navFixed.addClass("fixed").removeClass("header__nav--index not-fixed");
+	};
+
+	if (navFixed.length) {
+		$(window).scroll(function(){
+			var scroll = $(window).scrollTop();
+			if (scroll < 120) {
+				navFixed.addClass("not-fixed header__nav--index");
+				navFixed.removeClass("fixed hidden");
+			};
+			if (scroll > 110){
+				navFixed.addClass("hidden");
+			};
+			if (scroll > 120){
+				navFixed.addClass("fixed");
+				navFixed.removeClass("header__nav--index not-fixed hidden");
+			};
+		});
+	};
 
 
-    // google map hide/show full height
-    $('#map-canvas').height($(window).height());
-	var viewMap = $('.map__open').text();
-    $('.map__open').on('click', function(){
-    	if ($('.map').hasClass('open')) {
-    		$('.map').removeClass('open');
-    		$(this).text(viewMap);
-    		// $('#map-canvas')
-    	} else {
-	    	$('.map').addClass('open');
-	    	$(this).text('Свернуть карту');
-    	};
-    });	
+	// fixed menu
+	// if ($('.header__nav--index').length) {
+	// 	$(window).scroll(function(){
+	// 		var scroll = $(window).scrollTop();
+	// 		if (scroll > 150){
+	// 			$(".header__nav").addClass("hide");
+	// 		};
+	// 		if (scroll > 160){
+	// 			$(".header__nav").addClass("fixed");
+	// 			$(".header__nav").removeClass("header__nav--index");
+	// 		};
+	// 		if (scroll < $(".header__nav").height()) {
+	// 			$(".header__nav").removeClass("hide fixed");
+	// 			$(".header__nav").addClass("header__nav--index");
+	// 		};
+	// 	});
+	// };
 
-    // smooth scroll anchor
+
+
+
+	// google map hide/show full height
+	var mapIndexPage = $('.map--index'),
+		viewMap = $('.map__open').text();
+
+	mapIndexPage.find('#map-canvas').height(winH);
+	$('.map__open').on('click', function(){
+		if (mapIndexPage.hasClass('open')) {
+			mapIndexPage.removeClass('open');
+			$(this).text(viewMap);
+			// $('#map-canvas')
+		} else {
+			mapIndexPage.addClass('open');
+			$(this).text('Свернуть карту');
+		};
+	});
+
+	$('.map--full').height(winH - ($('.header').height() + $('.footer').height()))
+
+	// smooth scroll anchor
 	$('.anchor').on('click', function(){
-	    var scroll_el = $(this).attr('href');
-        if ($(scroll_el).length != 0) {
-	    	$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 800);
-        }
-	    return false;
-    });
+		var scroll_el = $(this).attr('href');
+		if ($(scroll_el).length != 0) {
+			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 800);
+		}
+		return false;
+	});
 
 		// var glide = $('.slider').glide({
 		// 	arrowRightText: '',
