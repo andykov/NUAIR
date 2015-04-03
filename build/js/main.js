1864647bdbe48706bea513930eefb2a5b12244bb
Andrey Budnikov <andykov.work@gmail.com>
2015-04-03 03:03:40 +0300

    Правки

--- build/js/main.js
+++ build/js/main.js
@@ -65,14 +65,25 @@ $(function() {
 			};
 		});
 	};
-
+	if (navFixed.length) {
+		$(window).scroll(function(){
+			var scroll = $(window).scrollTop();
+			if (scroll < 120) {
+				console.log('1');
+				// navFixed.addClass("not-fixed");
+				// navFixed.removeClass("fixed hidden");
+				// navFixedIndex.addClass("header__nav--index");
+			};
+		});
+	};
 	// google map open/closed full height
 	var mapIndex = $('.map--index'),
 		viewMap = $('.map__open').text(),
 		mapIndexHeightDef = $('.map--index').height();
 
 	mapIndex.find('#map-canvas').height(winH - $('.footer').height());
-	mapIndex.find('.map__open').on('click', function(){
+	mapIndex.find('.map__open').on('click', function(e){
+		e.preventDefault();
 		if (!mapIndex.hasClass('open')) {
 			mapIndex.height(winH - $('.footer').height()).addClass('open');
 			$(this).text('Свернуть карту');
@@ -88,11 +99,13 @@ $(function() {
 
 	// smooth scroll anchor
 	$('.anchor').on('click', function(){
-		var scroll_el = $(this).attr('href');
+		var $this = $(this),
+			scroll_el = $this.attr('href');
 		if ($(scroll_el).length != 0) {
-			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 800);
+			// $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 800);
+			var anchorCenter = (winH - $(scroll_el).height()) / 2;
+			$('html, body').animate({ scrollTop: $(scroll_el).offset().top - anchorCenter }, 800);
 		}
 		return false;
 	});
-
 }); // end jquery
